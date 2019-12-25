ffeeb2fdf8a197cd4ccda2ba76af4e7e149d051f
Andrew Shumakov <43216648+AndrewShumakov@users.noreply.github.com>
2019-12-26 00:20:22 +0300

    Update train.py

--- train.py
+++ train.py
@@ -24,7 +24,7 @@ parser = argparse.ArgumentParser()
 # data I/O
 parser.add_argument('-i', '--data_dir', type=str, default='/local_home/tim/pxpp/data', help='Location for the dataset')
 parser.add_argument('-o', '--save_dir', type=str, default='/local_home/tim/pxpp/save', help='Location for parameter checkpoints and samples')
-parser.add_argument('-d', '--data_set', type=str, default='cifar', help='Can be either cifar|imagenet|malaria')
+parser.add_argument('-d', '--data_set', type=str, default='cifar', help='Can be either cifar|imagenet|stl')
 parser.add_argument('-t', '--save_interval', type=int, default=20, help='Every how many epochs to write checkpoint/samples?')
 parser.add_argument('-r', '--load_params', dest='load_params', action='store_true', help='Restore training from previous model checkpoint?')
 # model
@@ -70,9 +70,9 @@ if args.data_set == 'cifar':
 elif args.data_set == 'imagenet':
     import data.imagenet_data as imagenet_data
     DataLoader = imagenet_data.DataLoader
-elif args.data_set == 'malaria':
-    import data.malaria_data as malaria_data
-    DataLoader = malaria_data.DataLoader
+elif args.data_set == 'stl':
+    import data.stl10_data as stl_data
+    DataLoader = stl_data.DataLoader
 else:
     raise("unsupported dataset")
 train_data = DataLoader(args.data_dir, 'train', args.batch_size * args.nr_gpu, rng=rng, shuffle=True, return_labels=args.class_conditional)
